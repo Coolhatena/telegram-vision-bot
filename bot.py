0fff77213384bc1551fc7aa4f398285c7e606df9
Coolhatena <ingsc.geffroy@gmail.com>
2025-03-01 22:27:55 -0800

    Change message sent to the user

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}!')
 
 
 app = ApplicationBuilder().token("7681957954:AAHNv7K4oyjaUsXShouNMopsvM9i17qJ5OE").build()
